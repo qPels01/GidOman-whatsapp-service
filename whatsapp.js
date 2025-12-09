275839bf0d611b89b990dbce112345e67c8342ba
qPels01 <petrlensin011@gmail.com>
2025-12-10 01:19:23 +0300

    whatsapp test, succsess

--- whatsapp.js
+++ whatsapp.js
@@ -1,5 +1,29 @@
 import { GreenApiClient } from '@green-api/whatsapp-api-client-js-v2';
+import dotenv from 'dotenv';
+import { getImageDataAsBase64 } from './utils/filePathGenerator.js';
+
+dotenv.config();
 
 const client = new GreenApiClient({
-    
+    idInstance: process.env.WHATSAPP_API_INSTANCE_ID,
+    apiTokenInstance: process.env.WHATSAPP_API_TOKEN_INSTANCE
+});
+
+const phoneNumber = '79103577107';
+const imgURL = getImageDataAsBase64()
+
+// const res = await client.sendMessage({
+//     chatId: `${phoneNumber}@c.us`,
+//     message: 'This is my message to my master:',
+// });
+
+const res = await client.sendFileByUpload({
+    chatId: `${phoneNumber}@c.us`,
+    file: {
+        data: imgURL,
+        fileName: '1.jpg'
+    },
+    caption: 'Мяу'
 })
+
+console.log(res)
